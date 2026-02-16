09801409319b604d723e50beba3388702605f4c7
Beccins <rebecca.munns@thefuneralbook.com>
2026-02-16 14:10:20 +1100

    Update page.tsx
    tidy up scripture reading

--- app/order-of-service/page.tsx
+++ app/order-of-service/page.tsx
@@ -27,10 +27,7 @@ const serviceItems: ServiceItem[] = [
   {
     id: "Reading",
     title: "Scripture Reading; 1 Peter chapter 1:3-9",
-    subtitle: "Caitlyn Munns",
-    time: "unknown",
-    type: "general",
-    content: copy deck of scripture reading
+    subtitle: "Caitlyn Munns"
   },
   {
     id: "song",

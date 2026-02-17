16d9b12be3b183a7d14783625b9994ec2f76ed54
Beccins <rebecca.munns@thefuneralbook.com>
2026-02-18 10:49:53 +1100

    Update page.tsx
    updated code manually as per claudes instruction - will commit and see

--- app/order-of-service/page.tsx
+++ app/order-of-service/page.tsx
@@ -210,7 +210,7 @@ function InteractiveServiceItem({ item }: { item: ServiceItem }) {
             <iframe
                 data-testid="embed-iframe"
                 style={{ borderRadius: "12px" }}
-                src={`https://open.spotify.com/embed/track/${item.spotifyId}?utm_source=generator`}
+                src={https://open.spotify.com/embed/track/${item.spotifyId}?utm_source=generator`}`
                 width="100%"
                 height="352"
                 frameBorder="0"
